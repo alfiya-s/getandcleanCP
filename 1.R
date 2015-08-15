dd52b5c61fe8b73cea20eb2351859f4f5342b7e7
Alfiya Sagitova <fifiusha@gmail.com>
2015-08-15 16:05:39 +0300

    Load data

--- 1.R
+++ 1.R
@@ -2,23 +2,15 @@ read_dta <- function(samp) {
 pths <- paste0('./', samp, '/')
 a <- c('subject_', 'X_', 'y_')
 pths1 <- paste0(pths, a, samp, '.txt')
-dt1 <- lapply(pths1, scan, sep = c('', ' ', ' '))
-dt1 <- do.call(cbind, dt1)
+dt <- lapply(pths1, scan, sep = c('', ' ', ''))
+dt1 <- dt[[1]]
+dt2 <- dt[[3]]
+dt3 <- dt[[2]]
 
-a <- c('x', 'y', 'z')
-b <- c('body_acc_', 'body_gyro_', 'total_acc_')
-c <- expand.grid(b, a)
-pths2 <- paste0(pths, 'Inertial Signals/', c[, 1], c[, 2], '_', samp, '.txt')
-
-dt2 <- lapply(pths2, scan, sep = c('', ' ', ' '))
-dt2 <- do.call(cbind, dt2)
-dt <- data.frame(dt1, dt2)
-names(dt)[4:12] <- paste0(c[, 1], c[, 2])
-names(dt)[1:3] <- substr(a, 1, nchar(a)-1)
-dt
+dt4 <- readLines('activity_labels.txt')
+dt5 <- readLines('features.txt')
+list(dt1, dt2, dt3, dt4, dt5)
 }
 dt.t <- read_dta('test')
 dt.tr <- read_dta('train')
-dt <- rbind(dt.t, dt.tr) 
-as.numeric(dt$x)
-head
+dt <- rbind(dt.t, dt.tr)
